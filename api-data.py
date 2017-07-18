d1baab5ac6075a03269cbd75fe6c7fcfcaedbd50
shravancr95 <shravan.rad@gmail.com>
2017-07-18 09:01:33 -0700

    fetches location coordinates and sorrounding places of interest within a radius

--- api-data.py
+++ api-data.py
@@ -1,1 +1,10 @@
-print("hello world")
+#GOOGLEPLACES API = AIzaSyCiT_v1UiAVzG3Shd4c5RxXxFncu5_d9YU
+from flask import Flask
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+    return "Hello World!"
+
+if __name__ == "__main__":
+    app.run()
